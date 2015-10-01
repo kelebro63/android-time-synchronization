55a608749a78605d3945cfb92864d1866dc1b5d8
kelebro63 <kelebro63@gmail.com>
2015-10-01 21:41:39 +0300

    test1

---
+++ MyApplication/app/src/main/java/com/example/test/myapplication/dateTimeConverter/dateTimeConverter/UniversalDateTime.java
@@ -0,0 +1,34 @@
+package com.example.test.myapplication.dateTimeConverter.dateTimeConverter;
+
+import org.joda.time.DateTime;
+
+/**
+ * Created by Bistrov Alexey on 09.09.2015.
+ */
+public class UniversalDateTime {
+
+
+    public static Long displacementTime;
+    private DateTime serverTime;
+
+
+    private UniversalDateTime(DateTime serverTime) {
+        this.serverTime = serverTime;
+    }
+
+    public static UniversalDateTime universalDateTimeGetterFromServer(DateTime serverTime) {
+        return new UniversalDateTime(serverTime);
+    }
+
+    public static UniversalDateTime universalDateTimeGetterFromDevice(DateTime deviceTime) {
+        return displacementTime != null ? new UniversalDateTime(new DateTime(deviceTime.getMillis() - displacementTime)) : null;
+    }
+
+    public void CalculateDisplacementTime(DateTime deviceTime) {
+        displacementTime = deviceTime.getMillis() - serverTime.getMillis();
+    }
+
+    public DateTime getServerTime() {
+        return serverTime;
+    }
+}
